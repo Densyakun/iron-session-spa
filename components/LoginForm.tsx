cbcfe0671e87662eeb3ae4464d780ecec8a7c9fe
densyakun <densyakun1128@gmail.com>
2022-10-08 02:43:37 +0900

    Input type change to password

--- components/LoginForm.tsx
+++ components/LoginForm.tsx
@@ -39,7 +39,7 @@ export default function Form({ }) {
         }}>
           <label>
             <span>Type your password</span>
-            <input type="text" name="password" required />
+            <input type="password" name="password" required />
           </label>
 
           <button type="submit">Login</button>
